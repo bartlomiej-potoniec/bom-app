82e63306d5bd89ac9a06a2070b161e5074a7d05f
bartlomiej-potoniec <bartpot98@protonmail.com>
2024-04-14 00:45:36 +0200

    Create database core & global DB config settings

---
+++ src/app/Core/Database.php
@@ -0,0 +1,61 @@
+<?php
+
+namespace App\Core;
+
+
+class Database
+{
+    private string $host = DB_HOST;
+    private string $user = DB_USER;
+    private string $pass = DB_PASS;
+    private string $dbName = DB_NAME;
+
+    private PDO $dbh;
+    private PDOStatement $stmt;
+
+    public function __construct()
+    {
+        $dsn = 'mysql:host=' . $this->host . ';dbname=' . $this->dbName;
+
+        $options = [
+            PDO::ATTR_ERRMODE => PDO::ERRMODE_EXCEPTION,
+        ];
+
+        try {
+            $this->dbh = new PDO($dsn, $this->user, $this->pass, $options);
+        } catch(PDOException $e) {
+            echo $e->getMessage();
+        }
+    }
+
+    public function query(string $sql): void
+    {
+        $this->stmt = $this->dbh->prepare($sql);
+    }
+
+    public function bind(string $param, string $value, $type = null): void
+    {
+        if(is_null($type)) {
+            switch(true) {
+                case is_int($value):
+                    $type = PDO::PARAM_INT;
+                    break;
+                case is_bool($value):
+                    $type = PDO::PARAM_BOOL;
+                    break;
+                case is_null($value):
+                    $type = PDO::PARAM_NULL;
+                    break;
+                default:
+                    $type = PDO::PARAM_STR;
+            }
+        }
+
+        $this->stmt->bindValue($param, $value, $type);
+    }
+
+    public function execute(): bool
+    {
+        return $this->stmt->execute();
+    }
+}
