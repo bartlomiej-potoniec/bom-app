edf095d5de24c0ebc474895746157691373558da
bartlomiej-potoniec <bartpot98@protonmail.com>
2024-04-13 16:28:41 +0200

    Implement PSR-4 autoloading for Core classes

--- public/index.php
+++ public/index.php
@@ -1,1 +1,8 @@
 <?php
+
+require_once '../src/app/bootstrap.php';
+
+use App\Core\Router;
+
+
+$router = new Router();
---
+++ src/app/bootstrap.php
@@ -0,0 +1,9 @@
+<?php
+
+# Autoload Core Libraries
+spl_autoload_register(function ($class_name) {
+    $path = __DIR__ . '/../' . lcfirst($class_name) . '.php';
+    $path = str_replace('\\', '/', $path);
+
+    require_once $path;
+});
