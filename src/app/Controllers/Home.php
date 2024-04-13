4639365d652495735dc3a978b52717e3f035b169
bartlomiej-potoniec <bartpot98@protonmail.com>
2024-04-13 19:21:06 +0200

    Create router based on MVC and implement URI capturing

---
+++ src/app/Controllers/Home.php
@@ -0,0 +1,11 @@
+<?php
+
+namespace App\Controllers;
+
+
+class Home
+{
+    public function __construct() {}
+
+    public function index() {}
+}
--- src/app/Core/Router.php
+++ src/app/Core/Router.php
@@ -5,5 +5,81 @@ namespace App\Core;
 
 class Router
 {
-    public function __construct() {}
+    private const CONTROLLERS_PATH = '../src/app/Controllers/';
+
+    /**
+     * Name of currently requested controller
+     */
+    protected string $currentController = 'Home';
+
+    /**
+     * Name of currently requested controller endpoint
+     */
+    protected string $currentMethod = 'index';
+
+    /**
+     * Params of the current URI request
+     *
+     * @var string[]
+     */
+    protected array $currentParams = [];
+
+    public function __construct()
+    {
+        $uri = $this->getUri();
+
+        if (isset($uri[1])) {
+            $this->setController($uri[1]);
+            unset($uri[1]);
+        }
+
+        require_once self::CONTROLLERS_PATH . $this->currentController . '.php';
+
+        $classNamespace = 'App\\Controllers\\' . $this->currentController;
+        $classAlias = 'Controller';
+
+        class_alias($classNamespace, $classAlias);
+        $controller = new $classAlias();
+
+        if (isset($uri[2])) {
+            $this->setMethod($controller, $uri[2]);
+            unset($uri[2]);
+        }
+
+        call_user_func([$controller, $this->currentMethod]);
+    }
+
+    private function setController(string $controllerName): void
+    {
+        $controllerPath = self::CONTROLLERS_PATH . $controllerName . '.php';
+
+        if (file_exists($controllerPath)) {
+            $this->currentController = ucfirst($controllerName);
+        }
+    }
+
+    private function setMethod(object $controller, string $methodName): void
+    {
+        if (method_exists($controller, $methodName)) {
+            $this->currentMethod = $methodName;
+        }
+    }
+
+    /**
+     * Gets current URI path.
+     *
+     * Parses the current URI path and returns an array representing its components.
+     * - $uri[0] is the main app root name.
+     * - $uri[1] is the current controller.
+     * - $uri[2] is the current controller method.
+     *
+     * @return string[] An array representing the components of the current URI path.
+     */
+    private function getUri(): array
+    {
+        $uri_path = parse_url($_SERVER['REQUEST_URI'], PHP_URL_PATH);
+        $uri = explode('/', trim($uri_path, '/'));
+
+        return $uri;
+    }
 }
