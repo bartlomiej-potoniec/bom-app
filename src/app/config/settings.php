b777e21c05a1e37ca67578c4eeec405aa8103edb
bartlomiej-potoniec <bartpot98@protonmail.com>
2024-04-14 17:24:50 +0200

    Add URL_ROOT inside config settings file

--- src/app/config/settings.php
+++ src/app/config/settings.php
@@ -1,10 +1,13 @@
 <?php
 
-// DB Params
+# DB Params
 define('DB_HOST', 'localhost');
 define('DB_USER', 'root');
 define('DB_PASS', '');
 define('DB_NAME', 'bom_db');
 
-// App Directory Root
+# App Directory Root
 define('APP_ROOT', dirname(dirname(__FILE__)));
+
+# URI Root
+define('URL_ROOT', 'http://localhost/bom-app');
